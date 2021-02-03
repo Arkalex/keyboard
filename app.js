0bdcc0f649bb758c04f7c13e4a21350b2a88ba1e
Alex Ayllon <alex.ayllon@lecreuset.com>
2021-02-03 12:27:48 +0100

    Replaced keyCode (deprecated) with code

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-window.addEventListener('keypress', (e) => {
-    let keyPressed = document.getElementsByClassName(`k${e.keyCode}`);
-    console.log(e.keyCode);
+window.addEventListener('keydown', (e) => {
+    let keyPressed = document.getElementsByClassName(`k${e.code}`);
     keyPressed[0].classList.add('pressed');
 })
 window.addEventListener('keyup', (e) => {
-    let keyPressed = document.getElementsByClassName(`k${e.keyCode}`)
+    let keyPressed = document.getElementsByClassName(`k${e.code}`);
+    console.log(e.code);
     keyPressed[0].classList.remove('pressed');
 })
