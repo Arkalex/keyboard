fa465621993131e194d54eb15412aac62beb71a6
Alex Ayllon <alex.ayllon@lecreuset.com>
2021-02-03 12:50:13 +0100

    Managed TAB to avoid navigation

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 window.addEventListener('keydown', (e) => {
     let keyPressed = document.getElementsByClassName(`k${e.code}`);
     keyPressed[0].classList.add('pressed');
+    e.code == 'Tab' && e.preventDefault();
 })
 window.addEventListener('keyup', (e) => {
     let keyPressed = document.getElementsByClassName(`k${e.code}`);
-    console.log(e.code);
     keyPressed[0].classList.remove('pressed');
 })
