b9787b27852c261656b362a1fe2fa1df6c3bff50
Alex Ayllon <alex.ayllon@lecreuset.com>
2021-02-05 12:33:53 +0100

    Select to change style

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
 const click = new Audio('./sounds/click.mp3');
 
 window.addEventListener('keydown', (e) => {
-    let keyPressed = document.getElementsByClassName(`k${e.code}`);
+    const keyPressed = document.getElementsByClassName(`k${e.code}`);
     click.play();
     keyPressed[0].classList.add('pressed');
     e.code == 'Tab' && e.preventDefault();
 })
 window.addEventListener('keyup', (e) => {
-    let keyPressed = document.getElementsByClassName(`k${e.code}`);
+    const keyPressed = document.getElementsByClassName(`k${e.code}`);
     keyPressed[0].classList.remove('pressed');
 })
+
+
+document.getElementById('select-style').addEventListener('change', (e) => {
+    const newStyle = e.target.value;
+    const rel = document.querySelector('link[theme="style"]')
+    switch(newStyle){
+        case 'Base':
+            rel.href = './styles/base.css';
+            break;
+        case 'Original':
+            rel.href = './styles/og.css';
+            break;
+    }
+});
